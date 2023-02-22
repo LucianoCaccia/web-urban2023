f6e2443366b6cc2fb1a05e22ff7a92fc1d15993e
LC <lcaccia@matterport.com>
2023-02-22 20:19:40 -0300

    #17 Adding custom fields on project carousel cards

--- wp/wp-content/themes/ue-theme/page-home.php
+++ wp/wp-content/themes/ue-theme/page-home.php
@@ -45,140 +45,50 @@ the_post();
 </div><!-- /#post-<?php the_ID(); ?> -->
 
 <section id="proyectos-slider">
-	<!-- Projectos Loop -->
 		<?php $args = array(
     		'post_type' => 'proyecto'
     		// 'posts_per_page' => 3
 			);
 		$the_query = new WP_Query( $args ); ?>
 
+	<div id="carouselExampleControls" class="carousel" data-bs-ride="carousel">
+		<div class="carousel-inner">
+		<!-- Projectos Loop -->
 		<?php if ( $the_query->have_posts() ) : ?>
     	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-			<a href="<?php the_permalink();?>">	<h2><?php the_title(); ?></h2> </a>
-			<p><?php the_content(); ?></p>
-			<?php $id = get_the_ID() ?>
-			<span class="badge text-bg-success"><?php the_field('amenities'); ?> </span>
-			<span class="badge text-bg-warning"><?php the_field('avance'); ?></span>
-    	<?php endwhile; ?>
+			<!-- <a href="<?php the_permalink();?>">	<h2><?php the_title(); ?></h2> </a> -->
+			<!-- <p><?php the_content(); ?></p> -->
+			<!-- <?php $id = get_the_ID() ?> -->
+			<!-- <span class="badge text-bg-success"><?php the_field('amenities'); ?> </span> -->
+			<!-- <span class="badge text-bg-warning"><?php the_field('avance'); ?></span> -->
 
-    	<?php wp_reset_postdata(); ?>
-
-		<?php endif; ?>
-	<!-- End loop -->
-
-
-		<div id="carouselExampleControls" class="carousel" data-bs-ride="carousel">
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 1</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 2</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 3</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 4</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 5</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 6</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 7</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 8</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<div class="card">
-						<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
-						<div class="card-body">
-							<h5 class="card-title">Card title 9</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-								card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
+			<div class="carousel-item "> <!-- pls add active class on first -->
+				<div class="card">
+					<div class="img-wrapper"><img src="..." class="d-block w-100" alt="..."> </div>
+					<div class="card-body">
+						<h5 class="card-title"><?php the_title(); ?></h5>
+						<p class="card-text"><?php echo wp_trim_words( get_the_content(), 15, '...' ); ?></p>
+						<a href="<?php the_permalink();?>" class="btn btn-primary">Go <?php the_title(); ?></a>
 					</div>
 				</div>
 			</div>
-			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Previous</span>
-			</button>
-			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Next</span>
-			</button>
+    	<?php endwhile; ?>
+    	<?php wp_reset_postdata(); ?>
+		<?php endif; ?>
+
 		</div>
-	</section>
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+	<!-- End loop -->
+
+</section>
 
 <?php
 	// If comments are open or we have at least one comment, load up the comment template.
