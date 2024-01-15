5af54e242cbb49176e5aede37c57849876cb934c
LC <lcaccia@matterport.com>
2024-01-15 20:03:23 -0300

    update "laundry"

--- wp/wp-content/themes/ue-theme/single-proyecto.php
+++ wp/wp-content/themes/ue-theme/single-proyecto.php
@@ -61,7 +61,7 @@ if ( have_posts() ) {
 										echo('</div>');
 									echo('</div>');
 								break;
-								case 'loundry':
+								case 'laundry':
 									echo('<div class="item me-2">');
 									echo('<img src="/wp-content/themes/ue-theme/img/amenities/laundry.svg">');
 										echo('<div>');
